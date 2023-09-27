22b150789d949331a17339c4c7f68babf7e85865
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-27 17:33:17 +0330

    platform detecting

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 // What is ract native API: api is a component that does not show you anythings but do something for you like `Stylesheet`. <--comment
 // Stylesheet is a api than make a configure you css styles. <--comment
 // Stylesheet.create: this method create styles for you. <--comment
 // Stylesheet.compose: This method merges to object(style classes) to gether. <--comment
+
+// Platform: this api is a platform detector(web. andeoid, ios) and called like below code. <--comment
+// Platform.OS <--comment
+// Platform.OS detector is good for small deference betwqeen platforms like color, but if the deference are lagest this don't use this and read below comment. <--comment
+// Platform.select() <-comment
+// Also if the component between platforms ahve a basic deference again Platform.select() is not a good choise, follow below. <--comment
+// Create optimzed component for any platform and use react native To use them(in next commit). <--comment
 function App() {
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.textWhite}>CILIVO.COM</Text>
+        <Text style={styles.textWhite}>{Platform.OS}</Text>
       </View>
     </>
   );
@@ -17,11 +25,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "green",
+    backgroundColor: Platform.OS === "android" ? "green" : "blue",
   },
   textWhite: {
     color: "#fff",
-    fontSize: 35,
+    ...Platform.select({
+      android: {
+        fontSize: 35,
+        textDecorationLine: "underline",
+      },
+      ios: {
+        fontSize: 40,
+        textDecorationLine: "none",
+      },
+      web: {
+        fontSize: 40,
+        textDecorationLine: "none",
+      },
+    }),
   },
 });
 
