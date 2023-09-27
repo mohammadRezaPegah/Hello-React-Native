45a9e90c7d4485eacc0e6bf26f985f8d3caa64a4
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-27 14:02:06 +0330

    Initial and styling

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.bgGreen}>
+        <Text style={styles.textWhite}>CILIVO.COM</Text>
+      </View>
+    </>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  bgGreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "green",
+  },
+  textWhite: {
+    color: "#fff",
+    fontSize: 35,
   },
 });
+
+export default App;
