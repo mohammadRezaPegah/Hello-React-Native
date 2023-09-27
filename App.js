7d31f4778945a2ff145bfab290b6aa3bc19d8cab
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-27 17:15:19 +0330

    Stylesheet api

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
-
+// What is ract native API: api is a component that does not show you anythings but do something for you like `Stylesheet`. <--comment
+// Stylesheet is a api than make a configure you css styles. <--comment
+// Stylesheet.create: this method create styles for you. <--comment
+// Stylesheet.compose: This method merges to object(style classes) to gether. <--comment
 function App() {
   return (
     <>
-      <View style={styles.bgGreen}>
+      <View style={styles.container}>
         <Text style={styles.textWhite}>CILIVO.COM</Text>
       </View>
     </>
   );
 }
 const styles = StyleSheet.create({
-  bgGreen: {
+  container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
@@ -21,5 +24,16 @@ const styles = StyleSheet.create({
     fontSize: 35,
   },
 });
+
+// const container = StyleSheet.compose(
+//   {
+//     flex: 1,
+//     justifyContent: "center",
+//   },
+//   {
+//     alignItems: "center",
+//     backgroundColor: "green",
+//   }
+// );
 
 export default App;
