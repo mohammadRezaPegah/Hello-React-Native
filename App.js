f321d2905e270f8c1b357154a4208babbbe58850
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-28 15:48:01 +0330

    oriantation

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
-import {
-  StyleSheet,
-  SafeAreaView,
-  StatusBar,
-  Button,
-  Dimensions,
-} from "react-native";
+import { StyleSheet, SafeAreaView, StatusBar, View } from "react-native";
 import { useState } from "react";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
-// Dimensions
-// With this api you can get device size
-// .get('screen') all of screen size
-// .get('window') aplication window size
-// Notic: this api on orientation does not show action.
+// To find phone oriantation we most use outside api(you can find it in reactnative.directory) and called by react-native-community/hooks.
 function App() {
   const [modaleVisible, setModalVisible] = useState(false);
+  const orientation = useDeviceOrientation();
   return (
     <>
       <SafeAreaView style={styles.container}>
         <StatusBar hidden={true} />
-        <Button
-          title="press"
-          onPress={() => {
-            alert(JSON.stringify(Dimensions.get("screen")));
+        <View
+          style={{
+            width: "100%",
+            height: orientation == "portrait" ? "30%" : "100%",
+            backgroundColor: "black",
           }}
-        />
+        ></View>
       </SafeAreaView>
     </>
   );
@@ -32,8 +25,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     color: "white",
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    // alignItems: "center",
     backgroundColor: "white",
   },
 });
