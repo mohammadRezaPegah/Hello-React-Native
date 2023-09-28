28bbbc71591ff5229228bcd5941ebc26eabfec04
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-28 11:28:15 +0330

    system detector

--- App.js
+++ App.js
@@ -11,11 +11,15 @@ import { StyleSheet, Text, View, Platform } from "react-native";
 // Also if the component between platforms ahve a basic deference again Platform.select() is not a good choise, follow below. <--comment
 // Create optimzed component for any platform and use react native To use them(native file extentions). <--comment
 // You just need to make a directory and make components with equal name for any plat form with the platform extionstion(test.android.js) and import the class(below code) and react native will detect to what file most be load on any platform. <--comment
-import Home from "./home";
+// import Home from "./home";
+
 function App() {
   return (
     <>
-      <Home></Home>
+      <View style={styles.container}>
+        <Text style={styles.textWhite}>CILIVO.COM</Text>
+        <Text style={styles.textWhite}>{Platform.OS}</Text>
+      </View>
     </>
   );
 }
---
+++ home/index.ios.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View, Platform } from "react-native";
+function Home() {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.textWhite}>Hello IOS!</Text>
+      </View>
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Platform.OS === "android" ? "green" : "blue",
+  },
+  textWhite: {
+    color: "#fff",
+    fontSize: 35,
+    textDecorationLine: "underline",
+  },
+});
+
+export default Home;
