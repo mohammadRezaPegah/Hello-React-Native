d53227a4e3c5f0e499a2cd9df787969918163507
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-28 16:21:49 +0330

    felx direction

--- App.js
+++ App.js
@@ -7,10 +7,18 @@ const App = () => {
           backgroundColor: "white",
           flex: 1,
           flexDirection: "row",
+          justifyContent: "center",
         }}
       >
-        <View style={{ backgroundColor: "blue", width: 100, height: 100 }} />
-        <View style={{ backgroundColor: "red", width: 100, height: 100 }} />
+        <View
+          style={{
+            backgroundColor: "blue",
+            width: 100,
+            height: 300,
+            alignSelf: "flex-start",
+          }}
+        />
+        <View style={{ backgroundColor: "red", width: 100, height: 200 }} />
         <View style={{ backgroundColor: "brown", width: 100, height: 100 }} />
       </View>
     </>
