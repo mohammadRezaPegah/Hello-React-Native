557eb12901828f29abbd32ee58e9e1648d97094c
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-28 15:09:48 +0330

    modal

--- App.js
+++ App.js
@@ -5,34 +5,57 @@ import {
   Pressable,
   Text,
   Alert,
+  Modal,
+  View,
+  Image,
 } from "react-native";
-
+import { useState } from "react";
 // Alert
 // Exams:
 // Alert.alert('title', 'message', btnArray)
 // Alert.prompt('title', 'message', callBack function) **just work on IOS**
+
+// Modal
+// First of all u need a state
 function App() {
+  const [modaleVisible, setModalVisible] = useState(false);
   return (
     <>
       <SafeAreaView style={styles.container}>
         <StatusBar hidden={true} />
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modaleVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed");
+            setModalVisible(!modaleVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Image
+                source={{ uri: "https://picsum.photos/200/200" }}
+                style={{ width: 200, height: 200, marginBottom: 10 }}
+              ></Image>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => {
+                  setModalVisible(!modaleVisible);
+                }}
+              >
+                <Text style={styles.textStyle}>Close!</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
         <Pressable
           style={[styles.button, styles.buttonOpen]}
           onPress={() => {
-            Alert.alert("title!", "message", [
-              {
-                text: "Yes",
-                onPress: () => {},
-              },
-              {
-                text: "No",
-                onPress: () => {},
-              },
-            ]);
-            // Alert.prompt("title!", "message", (text) => alert(text));
+            setModalVisible(!modaleVisible);
           }}
         >
-          <Text style={styles.textStyle}>Press here!</Text>
+          <Text style={styles.textStyle}>Modal show!</Text>
         </Pressable>
       </SafeAreaView>
     </>
@@ -54,11 +77,37 @@ const styles = StyleSheet.create({
   buttonOpen: {
     backgroundColor: "pink",
   },
+  buttonClose: {
+    backgroundColor: "red",
+  },
   textStyle: {
     fontSize: 20,
     color: "white",
     fontWeight: "bold",
     textAlign: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 5,
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 20,
+    alignItems: "center",
+    // Andeoid
+    elevation: 4,
+
+    // IOS
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
   },
 });
 
