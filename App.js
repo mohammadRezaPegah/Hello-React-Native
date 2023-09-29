3a5f67e62d4b6ca344180496b15815cd1a72c868
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-29 12:36:30 +0330

    Customize font

--- App.js
+++ App.js
@@ -1,30 +1,43 @@
+import { StyleSheet, ActivityIndicator } from "react-native";
+import { Text } from "react-native";
 import { View } from "react-native";
+import { useFonts } from "expo-font";
+
+// Customize font
+// If you wanna add customize font to the your application you most first of all installe below package.
+// expo-font
+// Add your customized font to the project and define that in the component
 const App = () => {
+  let [fontsloaded] = useFonts({
+    "iran-sanse": require("./assets/fonts/IRANSansWeb.ttf"),
+  });
+  if (!fontsloaded) {
+    return (
+      <>
+        <View style={styles.container}>
+          <ActivityIndicator size="large" color="black" />
+        </View>
+      </>
+    );
+  }
   return (
     <>
-      <View
-        style={{
-          backgroundColor: "white",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          flexWrap: "wrap",
-          alignContent: "center",
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "blue",
-            width: 100,
-            height: 100,
-          }}
-        />
-        <View style={{ backgroundColor: "red", width: 100, height: 100 }} />
-        <View style={{ backgroundColor: "brown", width: 100, height: 100 }} />
+      <View style={styles.container}>
+        <Text style={styles.text}>سیلیوو - ارتباط بدون واسطه</Text>
       </View>
     </>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontFamily: "iran-sanse",
+  },
+});
 
 export default App;
