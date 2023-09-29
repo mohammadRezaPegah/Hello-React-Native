b52e38a7e7dcfb2b0b5ac1665f24c505a22eec0a
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-29 14:26:24 +0330

    Icons

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StyleSheet, ActivityIndicator } from "react-native";
 import { Text } from "react-native";
 import { View } from "react-native";
 import { useFonts } from "expo-font";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 // Customize font
 // If you wanna add customize font to the your application you most first of all installe below package.
 // expo-font
 // Add your customized font to the project and define that in the component
+
+// Icon
+// For add Icon you can use Ionicons from expo, the list of icons is in https://icons.expo.fyi.
 const App = () => {
   let [fontsloaded] = useFonts({
     "iran-sanse": require("./assets/fonts/IRANSansWeb.ttf"),
@@ -24,6 +28,7 @@ const App = () => {
     <>
       <View style={styles.container}>
         <Text style={styles.text}>سیلیوو - ارتباط بدون واسطه</Text>
+        <Ionicons name="md-home" size={30} color="green" />
       </View>
     </>
   );
