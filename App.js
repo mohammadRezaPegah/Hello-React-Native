4fb73cd902bec7f884fb06efe0382d8168b0d50a
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-30 12:53:11 +0330

    Locale notification

--- App.js
+++ App.js
@@ -1,54 +1,135 @@
-import { useEffect } from "react";
-import { View, Text } from "react-native";
+import { useState, useEffect, useRef } from "react";
+import { Text, View, Button, Platform } from "react-native";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 
-// Push notifications:
-// Here we use expo notification and import that here and also install and import expo device to detect our device(is real or VMware).
-// To send test notification use this url https://expo.dev/notifications
+// Local notifications:
+// To use locale notification follow the blow codes and comments.
 
+// notification Handler setting
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: true,
-    shouldSetBadge: false,
+    shouldSetBadge: true,
   }),
 });
+
 const App = () => {
-  const registerPushNotification = async () => {
-    try {
-      // Get permission for notification send
-      const { status } = await Notifications.requestPermissionsAsync();
-      if (status !== "granted") return;
-      // Get token
-      const token = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId: "9c1281d0-b257-49a1-ab21-a8e39c76172a",
-        })
-      ).data;
-      console.log("toke:", token);
-    } catch (error) {
-      console.log("Errors: ", error);
-    }
-  };
+  const [expoPushToken, setExpoPushToken] = useState(""); // Push notification token
+  const [notification, setNotification] = useState(false); // Notification
+  const notificationListener = useRef(); // notification listener
+  const responseListener = useRef(); // Response to listener
 
-  // Call notification setup in component didmount
+  // Run some code on component didmount
   useEffect(() => {
-    registerPushNotification();
+    // Push notification registration and set token
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
 
-    // Notification Handeler
-    Notifications.addNotificationReceivedListener((notification) => {
-      console.log(notification);
-    });
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        // console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
   }, []);
 
+  // return view
   return (
-    <>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text style={{ fontSize: 30 }}>Cilivo</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{" "}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{" "}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
       </View>
-    </>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
   );
 };
+
+// notification setting and creation
+async function schedulePushNotification() {
+  // send notification
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+// notification registration and sending
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  // android device
+  if (Platform.OS === "android") {
+    // set notification chanel and information
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+  if (Device.isDevice) {
+    // get permissions
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+
+    // permission
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId: "9c1281d0-b257-49a1-ab21-a8e39c76172a",
+      })
+    ).data;
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  return token;
+}
 
 export default App;
